384444ca1877a6fa9de79f7238b33eee563afc14
thatsnotrlght <jartiste23@gmail.com>
2023-08-27 20:06:10 -0400

    Added docstring & allowed user to choose file name

--- password_manager.py
+++ password_manager.py
@@ -1,35 +1,48 @@
 master_password = input("What would you like your master password to be? ")
 
+userfile = input("What would you like your file name to be? ")
+
 # class exception to be raised
 class InvalidInput(Exception):
-    "Raised when the user does not input valid option"
+    """Raised when the user does not input a valid option"""
     pass
 
-
 #allows user to view existing passwords
-def view():
-    masterpwd = input("Please re-enter previously defined master password: ")
+def view(master_password):
+    """
+    Prompts the user to re-enter the master password and prints the stored passwords.
 
-    if masterpwd != master_password:
-        print("Incorrect master password, please try again")
-    else:
-        with open('passwords.txt', 'r') as file:
-            line = file.readline() #initial prime read for while loop
-            while line:
-                data = line.rstrip()
-                user, passw = data.split("|")
-                print("User:", user + " | " + "Password:", passw)
-                line = file.readline() #reassigns line to next line of file
-        
+    Args:
+        master_password (str): The master password to be verified.
+
+    Raises:
+        InvalidInput: If the entered master password does not match the predefined master password.
+
+    """
+    re_master_password = input("Please re-enter previously defined master password: ")
+
+    if re_master_password != master_password:
+        raise InvalidInput("Incorrect master password, please try again")
+
+    passwords = []
+    with open(userfile+".txt", 'r') as file:
+        for line in file:
+            user, passw = line.strip().split("|")
+            passwords.append({"User": user, "Password": passw})
+
+    print(passwords)
 
 #allows user to add a new password
 def add():
     name = input('Account name: ')
     pwd = input("Password: ")
-
-    #Manually closes file after use
-    with open('passwords.txt', 'a') as file:
-        file.write(name + " | " + pwd + '\n')
+    #Manually closes file after use with an exception handle
+    try:
+        with open(userfile+".txt", 'a') as file:
+            file.write(name + " | " + pwd + '\n')
+        return "Success"
+    except Exception as e:
+        print("An error occured while writing to the file:", str(e))
 
 
 while True:
@@ -39,7 +52,7 @@ while True:
 
     try:
         if user_option == "view":
-            view()
+            view(master_password)
         elif user_option == "add":
             add()
         else:
